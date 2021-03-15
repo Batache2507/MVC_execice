47b17f1bfb6f43a8b2ad76d4f481406b97e030cc
Nathan Bataille <nathan.bataille02@gmail.com>
2021-03-15 16:21:13 +0100

    last version exo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}));
 let router = require('./routes');
 app.use('/', router);
 
-//launch app to listen to specified port 
+//launchh app to listen to specified port 
 app.listen(8000, function (){
     console.log('Running on port 8000');
 })
